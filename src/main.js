60ab239d5ffa76aef993609abf79fac6ff490ea0
jonathanmaster <davidrodriguezcarvajal686@gmail.com>
2023-03-27 18:27:21 -0500

    version 1 terminando las mediacueries en el home

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const menuEmail = document.querySelector(".navbar-email");
+const menuCategory = document.querySelector(".menu-categories");
 const desktopMenu = document.querySelector(".desktop-menu");
+const desktopMenuCategory = document.querySelector(".desktop-menuCategory");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarIcon = document.querySelector(".navbar-shopping-cart");
@@ -17,6 +19,16 @@ menuEmail.addEventListener("click", () => {
   }
 
   desktopMenu.classList.toggle("inactive");
+});
+
+menuCategory.addEventListener("click", () => {
+  const isAsideClose = shoppingCartContainer.classList.contains("inactive"); //preguntando si tiene la clase entonces esta cerrado
+
+  if (!isAsideClose) {
+    shoppingCartContainer.classList.add("inactive");
+  }
+
+  desktopMenuCategory.classList.toggle("inactive");
 });
 
 menuHamIcon.addEventListener("click", () => {
@@ -60,129 +72,121 @@ productDetailCloseIcon.addEventListener("click", () => {
 
 //peticion a la api
 const api = axios.create({
-  baseURL: 'https://fakestoreapi.com/',
-  headers:{
-      "Content-Type": "aplication/json;charset=utf-8",
+  baseURL: "https://fakestoreapi.com/",
+  headers: {
+    "Content-Type": "aplication/json;charset=utf-8",
   },
-})
+});
 
 //All products
 //con axios no hay que poner data= res.json(), solo destructurando
-const getAllProducts = async()=>{
-const {data} = await api('products')
-data.forEach((product) => {
-  const productCardCategory = document.createElement("div");
-  productCardCategory.classList.add("product-card");
-
-  const productImg = document.createElement("img");
-  productImg.setAttribute("src", product.image);
-  productImg.addEventListener("click", () => {
-    shoppingCartContainer.classList.add("inactive");
-    productDetailContainer.classList.remove("inactive");
-  });
-
-  const productInfo = document.createElement('div')
-  productInfo.classList.add('product-info')
-
-  const productInfoDiv = document.createElement("div");
-
-      const productPrice = document.createElement("p");
-      productPrice.innerText = "$" + product.price;
-      const productName = document.createElement("p");
-      productName.innerText = product.title;
-      
-      productInfoDiv.appendChild(productPrice);
-      productInfoDiv.appendChild(productName);
-  
-      const productInfoFigure = document.createElement("figure");
-      const productImgCard = document.createElement("img");
-      productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
-  
-      productInfoFigure.appendChild(productImgCard);
-  
-      productInfo.appendChild(productInfoDiv);
-      productInfo.appendChild(productInfoFigure);
-  
-      productCardCategory.appendChild(productImg);
-      productCardCategory.appendChild(productInfo);
-  
-      cardsContainer.appendChild(productCardCategory);
-
-});
-
-}
-
-
-// algunos productos
-const getAllProductsHome = async()=>{
-  const {data} = await api('products?limit=6')
+const getAllProducts = async () => {
+  const { data } = await api("products");
   data.forEach((product) => {
     const productCardCategory = document.createElement("div");
-    productCardCategory.classList.add("product-card-2");
-  
+    productCardCategory.classList.add("product-card");
+
     const productImg = document.createElement("img");
     productImg.setAttribute("src", product.image);
     productImg.addEventListener("click", () => {
       shoppingCartContainer.classList.add("inactive");
       productDetailContainer.classList.remove("inactive");
     });
-  
-    const productInfo = document.createElement('div')
-    productInfo.classList.add('product-info-2')
-  
-    const productInfoDiv = document.createElement("div");
-  
-        const productPrice = document.createElement("p");
-        productPrice.innerText = "$" + product.price;
-        const productName = document.createElement("p");
-        productName.innerText = product.title;
-        
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productName);
-    
-        const productInfoFigure = document.createElement("figure");
-        const productImgCard = document.createElement("img");
-        productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
-    
-        productInfoFigure.appendChild(productImgCard);
-    
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-    
-        productCardCategory.appendChild(productImg);
-        productCardCategory.appendChild(productInfo);
-    
-        cardsContainer2.appendChild(productCardCategory);
-  
-  });
-  
-}
 
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.title;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCard = document.createElement("img");
+    productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCardCategory.appendChild(productImg);
+    productCardCategory.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCardCategory);
+  });
+};
+
+// algunos productos
+const getAllProductsHome = async () => {
+  const { data } = await api("products?limit=6");
+  data.forEach((product) => {
+    const productCardCategory = document.createElement("div");
+    productCardCategory.classList.add("product-card-2");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+    productImg.addEventListener("click", () => {
+      shoppingCartContainer.classList.add("inactive");
+      productDetailContainer.classList.remove("inactive");
+    });
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info-2");
+
+    const productInfoDiv = document.createElement("div");
+
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.title;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productImgCard = document.createElement("img");
+    productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCardCategory.appendChild(productImg);
+    productCardCategory.appendChild(productInfo);
+
+    cardsContainer2.appendChild(productCardCategory);
+  });
+};
 
 //Categories
-const getCategoriesProducts = async()=>{
+const getCategoriesProducts = async () => {
+  const { data } = await api("products/categories");
+  data.forEach((category) => {
+    const previewCategoryContainer = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
 
-const {data} = await api('products/categories')
-data.forEach((category) => {
-    
-  const previewCategoryContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
 
-  const categoryContainer = document.createElement('div')
-  categoryContainer.classList.add('category-container')
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.innerText =
+      category.charAt(0).toUpperCase() + category.slice(1);
 
-  const categoryTitle = document.createElement('h3')
-  categoryTitle.classList.add('category-title')
-  categoryTitle.innerText = category.charAt(0).toUpperCase() + category.slice(1);
+    const categoryImage = document.createElement("img");
+    categoryImage.classList.add("category-image");
+    categoryImage.setAttribute("src", categoryImages[category]);
 
-  const categoryImage = document.createElement('img');
-  categoryImage.classList.add('category-image');
-  categoryImage.setAttribute('src', categoryImages[category]);
-
-  categoryContainer.appendChild(categoryImage); 
-  categoryContainer.appendChild(categoryTitle)
-  previewCategoryContainer.appendChild(categoryContainer)
-});
-
-}
-
-
+    categoryContainer.appendChild(categoryImage);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoryContainer.appendChild(categoryContainer);
+  });
+};
--- src/navigation.js
+++ src/navigation.js
@@ -1,45 +1,37 @@
+const navigator = () => {
+  console.log({ location });
 
-const navigator = ()=>{
-    console.log({location})
-    
-    if (location.hash.startsWith('#allproducts')) {
-        allPage()
-    }else if (location.hash.startsWith('#details=')) {
-        detailsPage()
-    }else if (location.hash.startsWith('#category=')) {
-        categoriesPage()
-    }else{
-        homePage()
-    }
-    
-    location.hash
-}
+  if (location.hash.startsWith("#allproducts")) {
+    allPage();
+  } else if (location.hash.startsWith("#details=")) {
+    detailsPage();
+  } else if (location.hash.startsWith("#category=")) {
+    categoriesPage();
+  } else {
+    homePage();
+  }
+
+  location.hash;
+};
 
 window.addEventListener("DOMContentLoaded", navigator, false);
-window.addEventListener('hashchange', navigator,false)
+window.addEventListener("hashchange", navigator, false);
 
-const homePage = ()=>{
-    getCategoriesProducts()
-    getAllProductsHome()
-    console.log('home')
-}
+const homePage = () => {
+  getCategoriesProducts();
+  getAllProductsHome();
+  console.log("home");
+};
 
-const allPage = ()=>{
-    getAllProducts()
-    console.log('Estamos en allproducts')
-}
+const allPage = () => {
+  getAllProducts();
+  console.log("Estamos en allproducts");
+};
 
-const detailsPage = ()=>{
-    console.log('detalles producto')
-}
+const detailsPage = () => {
+  console.log("detalles producto");
+};
 
-const categoriesPage = ()=>{
-    console.log('categories')
-}
-
-
-
-
-
-
-
+const categoriesPage = () => {
+  console.log("categories");
+};
